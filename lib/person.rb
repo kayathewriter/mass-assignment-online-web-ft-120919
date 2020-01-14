0077277b872c8fbd3bb810242c4083953c657fba
Mikael Kaya <kayaswork@gmail.com>
2020-01-14 17:11:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
-class Person
-  #your code here
-end
+bob_attributes = {name: "Bob", hair_color: "Brown"}
+ 
+bob = Person.new(bob_attributes)
+bob.name       # => "Bob"
+bob.hair_color # => "Brown"
+ 
+susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+ 
+susan = Person.new(susan_attributes)
+susan.name     
+susan.height   
+susan.eye_color
